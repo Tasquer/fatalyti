01face6a646e35c3f41f4c8aa6fe6c84a0addbb7
tasquer <tasquerfdw@gmail.com>
2025-09-09 04:09:35 -0300

    creada vista y url para app1 en rama1

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index_app1(request):
+    return HttpResponse("<h1>Hola, esta es la App 1</h1>")
